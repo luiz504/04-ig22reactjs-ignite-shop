da5e44585de1623cdd095d8c05fd2f6e6316abe1
Luiz <l.bueno@xsensors.ai>
2023-05-18 20:41:23 -0300

    product page base structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,26 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetail,
+} from '~/styles/pages/product'
 
 export default function Product() {
-  const { query } = useRouter()
-  return <h1>Product {JSON.stringify(query)}</h1>
+  // const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageContainer />
+      <ProductDetail>
+        <h1>Product Name</h1>
+        <span>R$79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          doloribus recusandae aut itaque similique magnam ut distinctio alias,
+          ad quae veniam enim doloremque, vitae voluptatem quisquam. Magnam
+          sequi officiis tempora?
+        </p>
+        <button>Buy</button>
+      </ProductDetail>
+    </ProductContainer>
+  )
 }
