e7a23c20a71d7eee1fba17c11cbac6444f03569e
Luiz <l.bueno@xsensors.ai>
2023-05-18 08:59:54 -0300

    style: appContainer and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import logoFullSVG from '~/assets/logoFull.svg'
+
 import { globalStyles } from '~/styles/global'
+import { AppContainer, Header } from '~/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContainer>
+      <Header>
+        <Image src={logoFullSVG} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </AppContainer>
+  )
 }
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from '..'
+
+export const AppContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100svh',
+})
+
+export const Header = styled('header', {
+  padding: '2rem',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+})
